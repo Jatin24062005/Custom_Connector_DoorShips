0fdf27a6733e55e62c90e66865d80ec51608fe3f
JATIN <Jatin04072005@gmail.com>
2026-08-04 14:17:17 +0530

    fixing home page Opening

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -3,6 +3,11 @@ import { login } from "../../shopify.server";
 import styles from "./styles.module.css";
 
 export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+
+  if (url.searchParams.get("shop")) {
+    throw redirect(`/app?${url.searchParams.toString()}`);
+  }
 
   return { showForm: Boolean(login) };
 };
